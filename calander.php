7ee1517efda90a05efc3fea27df20c6dfad20130
thaindu <mahathun.onlne@gmail.om>
2016-04-01 12:00:41 +1300

    initial copy

---
+++ calander.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+Plugin Name: Event Calander -(TDM)
+Plugin URI: http://facebook.com/mahathun
+Description: This is plugin which elaborate an event calander.
+Author: Tharindu Mahavithana
+Version: 1.0
+Author URI: http://facebook.com/mahathun
+*/
+
+//usage: pr($avariable);
+// if (!function_exists('pr')) {
+//   function pr($var) { echo '<pre>'; var_dump($var); echo '</pre>';}
+// }
+
+
+
+
+// function showCalander(){
+// 	$days = date("t");
+
+// 	$dow = date("w", mktime(0,0,0,11,1,date("Y")));
+
+// 	$daycount = 1;
+
+// 	echo "$days $dow";
+// 	?>
+
+// 	<table border=1>
+
+// 		<th>Sun</th>
+// 		<th>Mon</th>
+// 		<th>Teu</th>
+// 		<th>Wed</th>
+// 		<th>Thu</th>
+// 		<th>Fri</th>
+// 		<th>Sat</th>
+
+// 		<tr>
+	
+	
+// 	<?php
+
+
+
+// 	for($i=0; $i<$days+$dow; $i++){
+
+// 		while($i <($days+$dow)){
+
+// 		}
+
+		
+// 	}
+
+// 		if($i>$dow)
+// 			echo "<td>".$daycount++."</td>";
+// 		else
+// 			echo '<td>&nbsp;</td>';
+
+
+// 		if($i %7 ==0)
+// 			echo "</tr>";
+
+// 		if($i <($days+$dow)){
+// 			echo "<tr>";
+// 		}
+
+// 	}
+
+// 	?>
+
+// 	</table>
+
+// <?php
+
+
+
+
+
+
+// //shortcodes
+// add_shortcode('viewTDMEventCalander','showCalander');
